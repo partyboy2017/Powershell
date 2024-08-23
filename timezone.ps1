cd07dc53f868d9afa070ee0185a3a98746696311
partyboy2017 <crjmorrison@gmail.com>
2024-08-23 09:08:57 -0400

    Update timezone.ps1

--- timezone.ps1
+++ timezone.ps1
@@ -1,3 +1,5 @@
-w32tm /config /manualpeerlist:"129.6.15.28  132.163.97.1" /syncfromflags:manual /update
+net stop w32time
+w32tm /config /manualpeerlist:"129.6.15.28, 132.163.97.1, 128.138.140.44" /syncfromflags:manual /update
 set-timezone -ID "Eastern Standard Time"
+net start w32time
 w32tm /resync
