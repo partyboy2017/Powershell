f391c3e4a068b5d9dbda829188725319d7d34040
partyboy2017 <crjmorrison@gmail.com>
2025-06-16 09:14:19 -0400

    Update Active Users.ps1

--- Active Users.ps1
+++ Active Users.ps1
@@ -1,2 +1,8 @@
-#get list of active users
-Get-ADUser -Filter {Enabled -eq $true} -Properties SamAccountName | Sort-Object -Property SamAccountName -Descending | Format-Table
+# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Retrieve all enabled (active) user accounts
+$activeUsers = Get-ADUser -Filter {Enabled -eq $true} -Properties SamAccountName
+
+# Display only the SamAccountName (username) of active accounts
+$activeUsers | Select-Object name,SamAccountName,userPrincipalName | Format-Table -AutoSize | out-file "[locations]\activeusers.txt"
